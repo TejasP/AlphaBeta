ae820b4f758bd992cebdaf5ef9859fdcda45c67c
TejasP <tparajia@hotmail.com>
2013-12-14 13:51:30 +0530

    Adding dashboard.

---
+++ app/Controller/DashboardController.php
@@ -0,0 +1,12 @@
+<?php
+
+App::uses('AuthController', 'Controller');
+
+
+class DashboardController extends AuthController {
+	var $uses = false;
+	public $helpers = array('Html', 'Form', 'Session');
+	
+	public function index() {
+	}
+}
